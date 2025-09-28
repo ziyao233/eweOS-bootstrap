b8a8dd036c95feacd487ab3dc4fcfdf1fa15e06a
Yao Zi <ziyao@disroot.org>
2025-09-28 16:47:13 +0000

    stage1: Add a shortcut target
    Signed-off-by: Yao Zi <ziyao@disroot.org>

--- stage1/stage1-targets.mk
+++ stage1/stage1-targets.mk
@@ -1,3 +1,11 @@
 $W/stage1.c-runtime: \
 	$W/stage1.musl-lib $W/stage1.musl-headers $W/stage1.compiler-rt
 	$(call done)
+
+$W/stage1: $W/stage1.c-runtime $W/stage1.cxx-runtime			\
+	$W/stage1.filesystem-layout					\
+	$W/stage1.bash $W/stage1.busybox $W/stage1.filesystem-layout	\
+	$W/stage1.make $W/stage1.pkgconf $W/stage1.mold $W/stage1.llvm	\
+	$W/stage1.ca-certs $W/stage1.curl $W/stage1.libarchive
+
+	$(call done)

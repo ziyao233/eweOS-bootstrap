e469658c4b34f742d47484c34eb51a2a0712cdb3
Yao Zi <ziyao@disroot.org>
2025-09-28 16:45:32 +0000

    stage1/libarchive: New recipe
    Signed-off-by: Yao Zi <ziyao@disroot.org>

---
+++ stage1/libarchive.mk
@@ -0,0 +1,24 @@
+$W/stage1.libarchive.extract: $W/LIBARCHIVE.download
+	tar xf $W/$(LIBARCHIVE_FILE) -C $W
+	$(call done)
+
+$W/stage1.libarchive: $W/stage1.libarchive.extract \
+	$W/stage1.c-runtime $W/stage1.zlib-ng $W/stage1.xz $W/stage1.openssl \
+	$W/stage1.zstd $W/stage1.acl
+
+	cd $W/libarchive-$(LIBARCHIVE_V) && \
+	$(call s1) ./configure --prefix=/usr \
+		--build=$(CBUILDHOST)		\
+		--host=$(CHOST)			\
+		--without-xml2			\
+		--without-lz4			\
+		--without-bz2lib		\
+		--without-expat			\
+		--with-sysroot=$O
+
+	+ $(call s1) make -C $W/libarchive-$(LIBARCHIVE_V)
+	+ $(call s1) make -C $W/libarchive-$(LIBARCHIVE_V) install DESTDIR="$O"
+
+	ln -s bsdtar "$O"/usr/bin/tar
+
+	$(call done)

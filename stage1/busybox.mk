8ff5f00780764cdc23eab631c8c1471bc726aafc
Yao Zi <ziyao@disroot.org>
2025-09-28 15:16:20 +0000

    stage1/busybox: Depend on filesystem-layout
    Busybox always install some symlinks to /usr/sbin, thus we want to delay
    the installation until the symlink for /usr/sbin is created.
    
    Signed-off-by: Yao Zi <ziyao@disroot.org>

--- stage1/busybox.mk
+++ stage1/busybox.mk
@@ -3,8 +3,11 @@ $W/stage1.busybox.extract: $W/BUSYBOX.download
 
 	$(call done)
 
+# Depends on filesystem-layout to ensure /usr/sbin has been installed as a
+# symlink before busybox installation
 $W/stage1.busybox: $W/stage1.busybox.extract $(SRCDIR)/stage1/busybox.config \
-	$W/stage1.c-runtime $W/stage1.linux-uapi-headers
+	$W/stage1.c-runtime $W/stage1.linux-uapi-headers		\
+	$W/stage1.filesystem-layout
 	sed -e "s|@PREFIX@|$O/usr|" $(SRCDIR)/stage1/busybox.config \
 		> $W/busybox-$(BUSYBOX_V)/.config
 
@@ -12,8 +15,5 @@ $W/stage1.busybox: $W/stage1.busybox.extract $(SRCDIR)/stage1/busybox.config \
 
 	+ $(call s1) make -C $W/busybox-$(BUSYBOX_V) CC=clang HOSTCC=clang \
 		V=1 install
-
-	mv $O/usr/sbin/* $O/usr/bin
-	rmdir $O/usr/sbin
 
 	$(call done)

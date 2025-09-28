220b425a17a349e01008b83dec8bf67b76d67744
Yao Zi <ziyao@disroot.org>
2025-09-28 14:45:37 +0000

    stage1/cmake-conf: Fix CMAKE_SYSTEM_PROCESSOR setting
    How could this work before???
    
    Signed-off-by: Yao Zi <ziyao@disroot.org>

--- stage1/cmake-conf.mk
+++ stage1/cmake-conf.mk
@@ -4,6 +4,6 @@ CMAKE_PROCESSOR_riscv64		:= riscv64
 CMAKE_PROCESSOR_loongarch64	:= loongarch64
 CMAKE_PROCESSOR			:= $(CMAKE_PROCESSOR_$(CARCH))
 
-$W/cmake-conf.cmake:
-	sed -e "s/%CHOST%/$(CHOST)/" $(SRCDIR)/stage1/cmake-conf.cmake \
-		> $W/cmake-conf.cmake
+$W/cmake-conf.cmake: $(SRCDIR)/stage1/cmake-conf.cmake
+	sed -e "s/%CHOST%/$(CHOST)/;s/%CCMAKEARCH%/$(CMAKE_PROCESSOR)/" \
+		$(SRCDIR)/stage1/cmake-conf.cmake > $W/cmake-conf.cmake

0d5ad6def0921265c966440d8ee9dff8f04f1217
Yao Zi <ziyao@disroot.org>
2025-09-30 05:33:05 +0000

    stage1/busybox: Add patch to fix non-x86_64 builds
    Signed-off-by: Yao Zi <ziyao@disroot.org>

--- stage1/busybox.mk
+++ stage1/busybox.mk
@@ -3,9 +3,15 @@ $W/stage1.busybox.extract: $W/BUSYBOX.download
 
 	$(call done)
 
+$W/stage1.busybox.patch: $W/stage1.busybox.extract
+	patch -p1 -d $W/busybox-$(BUSYBOX_V) < \
+		$(SRCDIR)/stage1/busybox-0001-fix-missing-sha1_process_block64_shaNI.patch
+
+	$(call done)
+
 # Depends on filesystem-layout to ensure /usr/sbin has been installed as a
 # symlink before busybox installation
-$W/stage1.busybox: $W/stage1.busybox.extract $(SRCDIR)/stage1/busybox.config \
+$W/stage1.busybox: $W/stage1.busybox.patch $(SRCDIR)/stage1/busybox.config \
 	$W/stage1.c-runtime $W/stage1.linux-uapi-headers		\
 	$W/stage1.filesystem-layout
 	sed -e "s|@PREFIX@|$O/usr|" $(SRCDIR)/stage1/busybox.config \

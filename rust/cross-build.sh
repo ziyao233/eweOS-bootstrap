ziyao233/eweOS-bootstrap

commit cff6240a8c56037e18a92f171080f9dfabd082ce

rust: cross: Initial support for 1.94.1
Signed-off-by: Yao Zi <me@ziyao.cc>

rust/cross-build.sh
@@ -1,0 +1,246 @@
+#!/usr/bin/env bash
+
+msg() {
+	echo "$1" 2>&1
+}
+
+if [ -t 1 ]; then
+	red_msg() {
+		echo -ne "\033[31m"
+		msg "$1"
+		echo -ne "\033[39m"
+	}
+
+	blue_msg() {
+		echo -ne "\033[34m"
+		msg "$1"
+		echo -ne "\033[39m"
+	}
+else
+	red_msg() {
+		msg "$1"
+	}
+
+	blue_msg() {
+		msg "$1"
+	}
+fi
+
+info() {
+	blue_msg "$1"
+}
+
+progress() {
+	blue_msg "$(date '+%C%y/%m/%d %H:%M:%S'): $1"
+}
+
+error() {
+	red_msg "$1"
+	exit 1
+}
+
+buildarch="$(uname -m)"
+hostarch="$1"
+case "$hostarch" in
+x86_64|aarch64|loongarch64)
+	;;
+riscv64)
+	hostarch="riscv64gc" ;;
+*)
+	error "Invalid hostarch \"$hostarch\"" ;;
+esac
+
+rustver="$2"
+
+patchdir="$(realpath $(dirname $0))"/patches/"$rustver"
+[ -d "$patchdir" ] || error "Invalid Rust version \"$rustver\""
+
+rootfs="$(realpath "$3")"
+[ "-d" "$rootfs" ] || error "Invalid rootfs \"$rootfs\""
+
+JOBS=${JOBS:-$(nproc)}
+
+progress "Cross-compiling Rust on $buildarch for $hostarch"
+msg	"Rust version:	$rustver"
+msg	"Rootfs:	$rootfs"
+msg	""
+
+progress "Downloading Rust source package version $rustver"
+
+curl -C - -o "rust-$rustver.tar.gz" -L \
+	"https://static.rust-lang.org/dist/rustc-$rustver-src.tar.gz" || exit 1
+
+progress "Uncompressing Rust source"
+tar xf "rust-$rustver.tar.gz"
+
+progress "Applying patches"
+for p in `find "$patchdir" -name '*.patch'`; do
+	info "Applying $p"
+	patch -p1 -d "rustc-$rustver-src" < "$p" || exit 1
+done
+
+progress "Generating Clang wrappers for host rootfs"
+cat <<EOF > clang-host
+#!/usr/bin/env bash
+exec clang --sysroot "$rootfs" 				\
+	-target $hostarch-unknown-linux-musl		\
+	-resource-dir="$rootfs/$(clang -print-resource-dir)" "\$@"
+EOF
+
+cat <<EOF > clangxx-host
+#!/usr/bin/env bash
+exec clang++ --sysroot "$rootfs" 				\
+	-target $hostarch-unknown-linux-musl			\
+	-nostdinc++ -isystem "$rootfs/usr/include/c++/v1"	\
+	-resource-dir="$rootfs/$(clang -print-resource-dir)" "\$@"
+EOF
+
+chmod +x clang-host clangxx-host
+
+arch2qemu() {
+	case "$1" in
+	x86_64)
+		echo "qemu-x86_64" ;;
+	aarch64)
+		echo "qemu-aarch64" ;;
+	riscv64)
+		echo "qemu-riscv64" ;;
+	loongarch64)
+		echo "qemu-loongarch64" ;;
+	esac
+}
+
+progress "Generate llvm-config wrappers for host LLVM"
+
+cat <<EOF > llvm-config-host
+#!/usr/bin/env bash
+
+hostconfig="$(arch2qemu "$hostarch") -L $rootfs $rootfs/usr/bin/llvm-config"
+\$hostconfig "\$@"
+EOF
+
+chmod +x llvm-config-host
+
+arch2llvm() {
+	case "$1" in
+	x86_64)
+		echo "X86" ;;
+	aarch64)
+		echo "AArch64" ;;
+	riscv64)
+		echo "RISCV" ;;
+	loongarch64)
+		echo "LoongArch" ;;
+	esac
+}
+
+buildllvmarch="$(arch2llvm $buildarch)"
+hostllvmarch="$(arch2llvm $hostarch)"
+
+progress "Generating Rust config.toml configuration"
+
+#	LLVM is devendored for both host and target, have tried,
+#
+#	## Devendor LLVM for build target only
+#
+#	Don't work. In opposite to obtaining flags correctly through host's
+#	llvm-config, Rustc instead insanely takes flags from BUILD's llvm-config
+#	and applies them for the HOST's LLVM, and states
+#
+#		This only really works if the host LLVM and target LLVM are
+#		compiled the same way, but for us that's typically the case.
+#
+#	in compiler/rustc_llvm/build.rs of Rust 1.94.1. Correct me if I'm wrong.
+#
+#	Unluckily the statement is wrong. Flags like "-I/usr/include" may be
+#	carried in llvm-config --cxxflags which pollutes header namespace and
+#	breaks build. They're common if Rust is built against a system LLVM
+#	installed to /usr, where -I{includedir} just expands to "/usr/include".
+#
+#	## Devendor LLVM for both build and host target (current way)
+#
+#	Works in a horrible way. LLVM could only be discovered by llvm-config
+#	or CMake configuration files, and Rust only looks for the former.
+#	However, llvm-config is an executable binary, not a script, making it
+#	extremely unsuitable for cross-compilation purpose.
+#
+#	In this bootstrapping process, we require qemu-user for the target
+#	architecture for configuring LLVM parameters.
+#
+#	See also https://github.com/llvm/llvm-project/issues/9777
+#
+#	Dawn. Good luck for anyone trying to fix it up. I always fall into this
+#	rabbit hole.
+cat <<EOF > config.toml
+profile = "user"
+
+[llvm]
+link-shared = true
+static-libstdcpp = false
+use-libcxx = true
+targets = "$buildllvmarch;$hostllvmarch"
+experimental-targets = ""
+
+[build]
+build = "$buildarch-unknown-linux-musl"
+host = ["$hostarch-unknown-linux-musl"]
+target = ["$hostarch-unknown-linux-musl"]
+cargo = "$(which cargo)"
+rustc = "$(which rustc)"
+rustfmt = "$(which rustfmt)"
+locked-deps = true
+vendor = true
+tools = ["cargo"]
+sanitizers = false
+profiler = false
+docs = false
+description = "eweOS rust bootstrap"
+
+[install]
+prefix = "/usr"
+
+[rust]
+debuginfo-level-std = 2
+channel = "stable"
+rpath = false
+backtrace-on-ice = true
+remap-debuginfo = true
+jemalloc = false
+llvm-libunwind = "system"
+codegen-units-std = 256
+deny-warnings = false
+lld = false
+musl-root = "/usr"
+llvm-tools = false
+
+[target.$buildarch-unknown-linux-musl]
+crt-static = false
+llvm-config = "$(which llvm-config)"
+
+[target.$hostarch-unknown-linux-musl]
+crt-static = false
+cc="$PWD/clang-host"
+cxx="$PWD/clangxx-host"
+linker="$PWD/clang-host"
+musl-root="$rootfs"
+musl-libdir="$rootfs/usr/lib"
+llvm-config = "$PWD/llvm-config-host"
+EOF
+
+progress "Building Rust"
+env \
+	RUST_BACKTRACE=1				\
+	PKG_CONFIG_PATH="$rootfs/usr/lib/pkgconfig"	\
+	PKG_CONFIG_SYSROOT_DIR="$rootfs"		\
+python "rustc-$rustver-src"/x.py build -j$JOBS || exit 1
+
+
+progress "Installing Rust toolchain files"
+DESTDIR="$PWD/install" python "$rustc-$rustver-src"/x.py install -j$JOBS ||
+	exit 1
+
+progress "Collecting toolchain tarball"
+tar --uid 0 --gid 0 --zstd -cf "rustc-$rustver-$hostarch.tar.zst" \
+	-C install . || exit 1
+
+ls -lh *.tar.zst

rust/patches/1.94.1/0001-musl-static.patch
@@ -1,0 +1,75 @@
+diff --git a/compiler/rustc_target/src/spec/base/linux_musl.rs b/compiler/rustc_target/src/spec/base/linux_musl.rs
+index 6d3124b..9321f45 100644
+--- a/compiler/rustc_target/src/spec/base/linux_musl.rs
++++ b/compiler/rustc_target/src/spec/base/linux_musl.rs
+@@ -3,9 +3,7 @@ use crate::spec::{Env, LinkSelfContainedDefault, TargetOptions, base, crt_object
+ pub(crate) fn opts() -> TargetOptions {
+     TargetOptions {
+         env: Env::Musl,
+-        pre_link_objects_self_contained: crt_objects::pre_musl_self_contained(),
+-        post_link_objects_self_contained: crt_objects::post_musl_self_contained(),
+-        link_self_contained: LinkSelfContainedDefault::InferredForMusl,
++        crt_static_default: false,
+         ..base::linux::opts()
+     }
+ }
+diff --git a/compiler/rustc_target/src/spec/crt_objects.rs b/compiler/rustc_target/src/spec/crt_objects.rs
+index 2d84e78..0e6cb0f 100644
+--- a/compiler/rustc_target/src/spec/crt_objects.rs
++++ b/compiler/rustc_target/src/spec/crt_objects.rs
+@@ -62,28 +62,6 @@ pub(super) fn all(obj: &'static str) -> CrtObjects {
+     ])
+ }
+ 
+-pub(super) fn pre_musl_self_contained() -> CrtObjects {
+-    new(&[
+-        (LinkOutputKind::DynamicNoPicExe, &["crt1.o", "crti.o", "crtbegin.o"]),
+-        (LinkOutputKind::DynamicPicExe, &["Scrt1.o", "crti.o", "crtbeginS.o"]),
+-        (LinkOutputKind::StaticNoPicExe, &["crt1.o", "crti.o", "crtbegin.o"]),
+-        (LinkOutputKind::StaticPicExe, &["rcrt1.o", "crti.o", "crtbeginS.o"]),
+-        (LinkOutputKind::DynamicDylib, &["crti.o", "crtbeginS.o"]),
+-        (LinkOutputKind::StaticDylib, &["crti.o", "crtbeginS.o"]),
+-    ])
+-}
+-
+-pub(super) fn post_musl_self_contained() -> CrtObjects {
+-    new(&[
+-        (LinkOutputKind::DynamicNoPicExe, &["crtend.o", "crtn.o"]),
+-        (LinkOutputKind::DynamicPicExe, &["crtendS.o", "crtn.o"]),
+-        (LinkOutputKind::StaticNoPicExe, &["crtend.o", "crtn.o"]),
+-        (LinkOutputKind::StaticPicExe, &["crtendS.o", "crtn.o"]),
+-        (LinkOutputKind::DynamicDylib, &["crtendS.o", "crtn.o"]),
+-        (LinkOutputKind::StaticDylib, &["crtendS.o", "crtn.o"]),
+-    ])
+-}
+-
+ pub(super) fn pre_mingw_self_contained() -> CrtObjects {
+     new(&[
+         (LinkOutputKind::DynamicNoPicExe, &["crt2.o"]),
+diff --git a/compiler/rustc_target/src/spec/targets/aarch64_unknown_linux_musl.rs b/compiler/rustc_target/src/spec/targets/aarch64_unknown_linux_musl.rs
+index 6ba5112..b66c225 100644
+--- a/compiler/rustc_target/src/spec/targets/aarch64_unknown_linux_musl.rs
++++ b/compiler/rustc_target/src/spec/targets/aarch64_unknown_linux_musl.rs
+@@ -14,9 +14,6 @@ pub(crate) fn target() -> Target {
+         | SanitizerSet::MEMORY
+         | SanitizerSet::THREAD;
+ 
+-    // FIXME(compiler-team#422): musl targets should be dynamically linked by default.
+-    base.crt_static_default = true;
+-
+     Target {
+         llvm_target: "aarch64-unknown-linux-musl".into(),
+         metadata: TargetMetadata {
+diff --git a/compiler/rustc_target/src/spec/targets/x86_64_unknown_linux_musl.rs b/compiler/rustc_target/src/spec/targets/x86_64_unknown_linux_musl.rs
+index ee88353..f65de3e 100644
+--- a/compiler/rustc_target/src/spec/targets/x86_64_unknown_linux_musl.rs
++++ b/compiler/rustc_target/src/spec/targets/x86_64_unknown_linux_musl.rs
+@@ -16,8 +16,6 @@ pub(crate) fn target() -> Target {
+         | SanitizerSet::MEMORY
+         | SanitizerSet::THREAD;
+     base.supports_xray = true;
+-    // FIXME(compiler-team#422): musl targets should be dynamically linked by default.
+-    base.crt_static_default = true;
+ 
+     Target {
+         llvm_target: "x86_64-unknown-linux-musl".into(),

rust/patches/1.94.1/0002-disable-no-default-libraries.patch
@@ -1,0 +1,12 @@
+diff --git a/compiler/rustc_target/src/spec/base/linux_musl.rs b/compiler/rustc_target/src/spec/base/linux_musl.rs
+index 9321f45..f2a0f37 100644
+--- a/compiler/rustc_target/src/spec/base/linux_musl.rs
++++ b/compiler/rustc_target/src/spec/base/linux_musl.rs
+@@ -4,6 +4,7 @@ pub(crate) fn opts() -> TargetOptions {
+     TargetOptions {
+         env: Env::Musl,
+         crt_static_default: false,
++        no_default_libraries: false,
+         ..base::linux::opts()
+     }
+ }

rust/patches/1.94.1/0003-drop-latomic-for-riscv-targets.patch
@@ -1,0 +1,67 @@
+From 5790860531e2fe601bbffcdc6bc7a5f192e8f593 Mon Sep 17 00:00:00 2001
+From: Yao Zi <ziyao@disroot.org>
+Date: Wed, 21 May 2025 09:45:11 +0000
+Subject: [PATCH] drop -latomic for riscv targets
+
+Rustc doesn't recognize compiler-rt based Linux distro correctly and
+tries to link libatomic on these targets. Let's drop the linkflag
+manually.
+
+Signed-off-by: Yao Zi <ziyao@disroot.org>
+---
+ compiler/rustc_llvm/build.rs               |  8 --------
+ src/bootstrap/src/core/build_steps/llvm.rs | 19 -------------------
+ 2 files changed, 27 deletions(-)
+
+diff --git a/compiler/rustc_llvm/build.rs b/compiler/rustc_llvm/build.rs
+index 6692ea735401..16f9738a4250 100644
+--- a/compiler/rustc_llvm/build.rs
++++ b/compiler/rustc_llvm/build.rs
+@@ -383,14 +383,6 @@ fn main() {
+         "stdc++"
+     };
+ 
+-    // RISC-V GCC erroneously requires libatomic for sub-word
+-    // atomic operations. Some BSD uses Clang as its system
+-    // compiler and provides no libatomic in its base system so
+-    // does not want this.
+-    if target.starts_with("riscv") && !target.contains("freebsd") && !target.contains("openbsd") {
+-        println!("cargo:rustc-link-lib=atomic");
+-    }
+-
+     // C++ runtime library
+     if !target.contains("msvc") {
+         if let Some(s) = llvm_static_stdcpp {
+diff --git a/src/bootstrap/src/core/build_steps/llvm.rs b/src/bootstrap/src/core/build_steps/llvm.rs
+index e21804fa3c07..884bf69459bf 100644
+--- a/src/bootstrap/src/core/build_steps/llvm.rs
++++ b/src/bootstrap/src/core/build_steps/llvm.rs
+@@ -399,25 +399,6 @@ impl Step for Llvm {
+             cfg.define("LLVM_LINK_LLVM_DYLIB", "ON");
+         }
+ 
+-        if (target.starts_with("csky")
+-            || target.starts_with("riscv")
+-            || target.starts_with("sparc-"))
+-            && !target.contains("freebsd")
+-            && !target.contains("openbsd")
+-            && !target.contains("netbsd")
+-        {
+-            // CSKY and RISC-V GCC erroneously requires linking against
+-            // `libatomic` when using 1-byte and 2-byte C++
+-            // atomics but the LLVM build system check cannot
+-            // detect this. Therefore it is set manually here.
+-            // Some BSD uses Clang as its system compiler and
+-            // provides no libatomic in its base system so does
+-            // not want this. 32-bit SPARC requires linking against
+-            // libatomic as well.
+-            ldflags.exe.push(" -latomic");
+-            ldflags.shared.push(" -latomic");
+-        }
+-
+         if target.starts_with("mips") && target.contains("netbsd") {
+             // LLVM wants 64-bit atomics, while mipsel is 32-bit only, so needs -latomic
+             ldflags.exe.push(" -latomic");
+-- 
+2.49.0
+

rust/patches/1.94.1/0004-bootstrap-Change-libexec-dir.patch
@@ -1,0 +1,39 @@
+From 0000000000000000000000000000000000000000 Mon Sep 17 00:00:00 2001
+From: "Jan Alexander Steffens (heftig)" <heftig@archlinux.org>
+Date: Thu, 6 May 2021 20:14:58 +0200
+Subject: [PATCH] bootstrap: Change libexec dir
+
+---
+ src/bootstrap/src/core/build_steps/dist.rs | 2 +-
+ src/bootstrap/src/core/build_steps/tool.rs | 2 +-
+ 2 files changed, 2 insertions(+), 2 deletions(-)
+
+diff --git a/src/bootstrap/src/core/build_steps/dist.rs b/src/bootstrap/src/core/build_steps/dist.rs
+index 83f71aeed720..2922054d6d60 100644
+--- a/src/bootstrap/src/core/build_steps/dist.rs
++++ b/src/bootstrap/src/core/build_steps/dist.rs
+@@ -432,7 +432,7 @@ fn prepare_image(builder: &Builder<'_>, compiler: Compiler, image: &Path) {
+                 },
+                 builder.kind,
+             ) {
+-                let dst = image.join("libexec");
++                let dst = image.join("lib");
+                 builder.install(&ra_proc_macro_srv.tool_path, &dst, FileType::Executable);
+             }
+ 
+diff --git a/src/bootstrap/src/core/build_steps/tool.rs b/src/bootstrap/src/core/build_steps/tool.rs
+index cd57e06ae04a..05d117eba66e 100644
+--- a/src/bootstrap/src/core/build_steps/tool.rs
++++ b/src/bootstrap/src/core/build_steps/tool.rs
+@@ -1122,9 +1122,9 @@
+             artifact_kind: ToolArtifactKind::Binary,
+         });
+ 
+-        // Copy `rust-analyzer-proc-macro-srv` to `<sysroot>/libexec/`
++        // Copy `rust-analyzer-proc-macro-srv` to `<sysroot>/lib/`
+         // so that r-a can use it.
+-        let libexec_path = builder.sysroot(self.compilers.target_compiler).join("libexec");
++        let libexec_path = builder.sysroot(self.compilers.target_compiler).join("lib");
+         t!(fs::create_dir_all(&libexec_path));
+         builder.copy_link(
+             &tool_result.tool_path,

rust/patches/1.94.1/0005-bootstrap-Build-tablegen-from-scratch.patch
@@ -1,0 +1,47 @@
+From 8fb6e48f874e93554aa5104eab9a10de8cf00056 Mon Sep 17 00:00:00 2001
+From: Yao Zi <me@ziyao.cc>
+Date: Sun, 26 Apr 2026 12:35:41 +0000
+Subject: [PATCH] bootstrap: llvm: Build llvm-tblgen from scratch
+
+Since the executable on the host might not match the version required
+to build Rust-shipped LLVM, causing compilation errors because of
+unrecognized arguments.
+
+Signed-off-by: Yao Zi <me@ziyao.cc>
+---
+ src/bootstrap/src/core/build_steps/llvm.rs | 12 ------------
+ 1 file changed, 12 deletions(-)
+
+diff --git a/src/bootstrap/src/core/build_steps/llvm.rs b/src/bootstrap/src/core/build_steps/llvm.rs
+index 1c15edc53dac..1deda69c8b02 100644
+--- a/src/bootstrap/src/core/build_steps/llvm.rs
++++ b/src/bootstrap/src/core/build_steps/llvm.rs
+@@ -484,10 +484,6 @@ impl Step for Llvm {
+                     .run_capture_stdout(builder)
+                     .stdout();
+                 let host_bin = Path::new(llvm_bindir.trim());
+-                cfg.define(
+-                    "LLVM_TABLEGEN",
+-                    host_bin.join("llvm-tblgen").with_extension(EXE_EXTENSION),
+-                );
+                 // LLVM_NM is required for cross compiling using MSVC
+                 cfg.define("LLVM_NM", host_bin.join("llvm-nm").with_extension(EXE_EXTENSION));
+             }
+@@ -1273,14 +1269,6 @@ impl Step for Lld {
+             .define("LLVM_CMAKE_DIR", llvm_cmake_dir)
+             .define("LLVM_INCLUDE_TESTS", "OFF");
+ 
+-        if !builder.config.is_host_target(target) {
+-            // Use the host llvm-tblgen binary.
+-            cfg.define(
+-                "LLVM_TABLEGEN_EXE",
+-                host_llvm_config.with_file_name("llvm-tblgen").with_extension(EXE_EXTENSION),
+-            );
+-        }
+-
+         cfg.build();
+ 
+         t!(lld_stamp.write());
+-- 
+2.53.0
+

commit e87ce34edaef28f7100b5148e86f296a56fd3405

rust: cross: Fix toolchain installation
Fixes: cff6240a8c56 ("rust: cross: Initial support for 1.94.1")
Signed-off-by: Yao Zi <me@ziyao.cc>

rust/cross-build.sh
@@ -228,15 +228,13 @@ llvm-config = "$PWD/llvm-config-host"
 EOF
 
 progress "Building Rust"
-env \
-	RUST_BACKTRACE=1				\
-	PKG_CONFIG_PATH="$rootfs/usr/lib/pkgconfig"	\
-	PKG_CONFIG_SYSROOT_DIR="$rootfs"		\
+export RUST_BACKTRACE=1
+export PKG_CONFIG_PATH="$rootfs/usr/lib/pkgconfig"
+export PKG_CONFIG_SYSROOT_DIR="$rootfs"
 python "rustc-$rustver-src"/x.py build -j$JOBS || exit 1
 
-
 progress "Installing Rust toolchain files"
-DESTDIR="$PWD/install" python "$rustc-$rustver-src"/x.py install -j$JOBS ||
+DESTDIR="$PWD/install" python "rustc-$rustver-src"/x.py install -j$JOBS ||
 	exit 1
 
 progress "Collecting toolchain tarball"